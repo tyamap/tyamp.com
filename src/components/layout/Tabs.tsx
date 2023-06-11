e306d7fc0caccf6750314c86e16a1376bec2a85c
tyamap <tom333my@outlook.com>
2023-06-11 21:03:56 +0900

    feat: RoutingTab component

---
+++ src/components/layout/Tabs.tsx
@@ -0,0 +1,45 @@
+import { Tabs as MantineTabs } from '@mantine/core'
+import { useLocation } from "@reach/router"
+import { navigate } from "gatsby"
+
+type Tab = { title: string, value: string }
+const tabs: Tab[] = [
+  { title: 'Overview', value: 'overview' },
+  { title: 'Projects', value: 'projects' },
+  { title: 'Works', value: 'works' },
+  { title: 'Posts', value: 'posts' },
+  { title: 'Contact', value: 'contact' },
+]
+const routeTab = tabs[0]
+
+interface TabsProps {
+}
+const Tabs = (props: TabsProps) => {
+  const location = useLocation()
+
+  return (
+    <MantineTabs color="cyan" defaultValue="overview"
+      value={location.pathname === '/'
+        ? routeTab.value
+        : location.pathname.replaceAll('/', '')
+      }
+      onTabChange={(value) => {
+        if (value === routeTab.value) {
+          navigate('/')
+        } else {
+          navigate(`/${value}`)
+        }
+      }}
+    >
+      <MantineTabs.List>
+        {tabs.map((tab) => (
+          <MantineTabs.Tab key={tab.value} value={tab.value}>
+            {tab.title}
+          </MantineTabs.Tab>
+        ))}
+      </MantineTabs.List >
+    </MantineTabs >
+  );
+};
+
+export default Tabs;
