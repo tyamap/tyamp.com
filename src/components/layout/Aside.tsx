3eab0f9dde30fe0f1becb9872116bcc4758d8b01
tyamap <tom333my@outlook.com>
2023-06-11 22:31:17 +0900

    fix: adjust Aside height

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -5,9 +5,9 @@ interface AsideProps {
 }
 const Aside = (props: AsideProps) => {
   return (
-    <MantineAside w={props.w} p="lg" withBorder={false} sx={{ borderRight: '1px solid #e9ecef' }}>
+    <MantineAside height="auto" w={props.w} p="lg" withBorder={false} sx={{ borderRight: '1px solid #e9ecef' }}>
       <MantineAside.Section>
-        <AspectRatio ratio={1} w="100%" mx="auto">
+        <AspectRatio ratio={1} w="100%" mx="auto" maw={360}>
           <Avatar radius="100%" size="100%" />
         </AspectRatio>
       </MantineAside.Section>
