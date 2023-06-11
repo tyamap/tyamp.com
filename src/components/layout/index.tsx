f2aa8e324830e1c4b0b1d801bc6fbb787cd8b70f
tyamap <tom333my@outlook.com>
2023-06-11 21:36:39 +0900

    feat: set Layout

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,33 @@
+import { Box, Container, Flex } from "@mantine/core"
+import { MantineProvider } from '@mantine/core';
+import { theme } from 'src/theme'
+import Header from "src/components/layout/Header";
+import Navbar from "src/components/layout/Aside";
+import Main from "src/components/layout/Main";
+import Tabs from "src/components/layout/Tabs";
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={theme}
+    >
+      <Header />
+      <Container size="xl" px={0}>
+        <Flex wrap="wrap">
+          <Navbar />
+          <Box w={{ md: '70%', xs: '100%' }}>
+            <Tabs />
+            <Main>
+              {children}
+            </Main>
+          </Box>
+        </Flex>
+      </Container>
+    </MantineProvider >
+  )
+}
