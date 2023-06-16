e75dfa62c00e01414347cafb03f37674448d3089
tyamap <tom333my@outlook.com>
2023-06-16 21:36:27 +0900

    ui: enhance skills view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Title, Text, Image } from "@mantine/core"
+import { Title, Text, Image, Flex, SimpleGrid } from "@mantine/core"
 import type { HeadFC, PageProps } from "gatsby"
 import React from "react"
 import { Layout } from "src/components/layout"
 import SEO from "src/components/SEO"
 import { useProfile } from "src/hooks/useProfile"
 
-const categories = ["Programming Languages", "Frameworks", "Tools", "Databases", "Clouds", "Others"]
+const categories = ["Programming Languages", "Frameworks", "Databases", "Tools", "Clouds", "Others"]
 const IndexPage: React.FC<PageProps> = () => {
   const profile = useProfile()
   return (
@@ -17,19 +17,27 @@ const IndexPage: React.FC<PageProps> = () => {
       {categories?.map((category) => (
         <React.Fragment>
           <Title key={category} order={3}>{category}</Title>
-          {profile?.skills?.filter((skill) => skill?.category === category)?.map((skill) => (
-            <React.Fragment key={skill?.name}>
-              <Image
-                src={skill?.icon?.file?.url || ""}
-                alt={`${skill?.name} icon`}
-                height={32} width={32}
-              />
-            </React.Fragment>
-          ))}
+          <SimpleGrid
+            cols={6}
+            breakpoints={[{ maxWidth: 'xs', cols: 3 }]}
+            my="sm"
+          >
+            {profile?.skills?.filter((skill) => skill?.category === category)?.map((skill) => (
+              <Flex direction="column" align="center" key={skill?.name}>
+                <Image
+                  src={skill?.icon?.file?.url || ""}
+                  alt={`${skill?.name} icon`}
+                  height={32} width={32}
+                />
+                <Text c="dimmed">{skill?.name}</Text>
+              </Flex>
+            ))}
+          </SimpleGrid>
         </React.Fragment>
-      ))}
+      ))
+      }
       <Title order={2}>Biography</Title>
-    </Layout>
+    </Layout >
   )
 }
 
