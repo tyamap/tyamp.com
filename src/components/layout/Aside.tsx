a5c0e0caf3737b78ea33310be485106de0902f24
tyamap <tom333my@outlook.com>
2023-06-20 21:04:23 +0900

    ui: enhance sp view
    close #4

--- src/components/layout/Aside.tsx
+++ src/components/layout/Aside.tsx
@@ -1,4 +1,4 @@
-import { Text, Aside as MantineAside, Title, AspectRatio, SpacingValue, SystemProp, Badge } from "@mantine/core"
+import { Text, Aside as MantineAside, Title, AspectRatio, Badge, Grid } from "@mantine/core"
 import { useProfile } from "src/hooks/useProfile";
 import { GatsbyImage } from 'gatsby-plugin-image'
 
@@ -7,20 +7,28 @@ interface AsideProps {
 const Aside = (props: AsideProps) => {
   const profile = useProfile()
   return (
-    <MantineAside h="auto" p="lg" withBorder={false} >
-      <MantineAside.Section>
-        <AspectRatio ratio={1} w="100%" mx="auto" maw={360}>
+    <MantineAside
+      h="auto"
+      p={{sx: "md", sm: 'lg'}}
+      withBorder={false}
+      sx={{
+        "@media (max-width: 767px)": {
+          flexDirection: 'row',
+          gap: 10
+        }
+      }}
+    >
+      <MantineAside.Section sx={{ flexBasis: "25%" }}>
+        <AspectRatio ratio={1} w="100%" maw={360}>
           <GatsbyImage
             image={profile!.avatar!.gatsbyImageData!}
             alt={profile?.avatar?.description || profile?.avatar?.title || 'avatar image'}
             style={{ borderRadius: '100%', width: '100%', zIndex: 1 }} />
         </AspectRatio>
       </MantineAside.Section>
-      <MantineAside.Section>
+      <MantineAside.Section sx={{ flexBasis: "75%" }}>
         <Title order={2}>{profile?.name}</Title>
         <Text c="dimmed">{profile?.userId}</Text>
-      </MantineAside.Section>
-      <MantineAside.Section>
         <Badge color="cyan">{profile?.label}</Badge>
       </MantineAside.Section>
     </MantineAside>
