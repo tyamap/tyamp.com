7109b0b9f9624070b81510853f9d2868012f5258
tyamap <tom333my@outlook.com>
2023-06-25 23:19:18 +0900

    ui: enhance aria

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Box, Header as MantineHeader, Text } from '@mantine/core'
+import { Box, Header as MantineHeader, Text } from '@mantine/core'
 import { IconMail } from '@tabler/icons-react';
 import { Link } from 'gatsby';
 import { useSiteMetadata } from 'src/hooks/useSiteMetadata';
@@ -17,13 +17,13 @@ const Header = (props: HeaderProps) => {
           '-webkit-background-clip': 'text',
           '-webkit-text-fill-color': 'transparent',
         })}>
-        <Link to="/" style={{ textDecoration: 'none' }}>
+        <Link to="/" aria-label='go to home' style={{ textDecoration: 'none' }}>
           <Text c="teal" fw={700} component='span'>
             {title}
           </Text>
         </Link>
       </Box>
-      <Link to="/contact">
+      <Link to="/contact" aria-label='go to contact page'>
         <IconMail color='steelblue' />
       </Link>
     </MantineHeader>
