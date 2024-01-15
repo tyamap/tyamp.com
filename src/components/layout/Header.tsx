a2f3c6d4e960563d2d1593b50630ed08be6fe10b
tyamap <tom333my@outlook.com>
2024-01-15 23:10:52 +0900

    feat: 言語切り替えリンク

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,62 @@
-import { Box, Header as MantineHeader, Text } from '@mantine/core'
-import { IconMail } from '@tabler/icons-react';
-import { Link } from 'gatsby';
-import { useSiteMetadata } from 'src/hooks/useSiteMetadata';
+import { Box, Header as MantineHeader, Text } from "@mantine/core";
+import { IconMail } from "@tabler/icons-react";
+import { Link } from "gatsby";
+import { useSiteMetadata } from "src/hooks/useSiteMetadata";
 
-interface HeaderProps {
-
-}
+interface HeaderProps {}
 const Header = (props: HeaderProps) => {
-  const { title } = useSiteMetadata()
+  const { title } = useSiteMetadata();
 
   return (
-    <MantineHeader height={50} p="sm" fixed display="flex" sx={{ justifyContent: "space-between" }}>
-      <Box display="inline-block"
+    <MantineHeader
+      height={50}
+      p="sm"
+      fixed
+      display="flex"
+      sx={{ justifyContent: "space-between" }}
+    >
+      <Box
+        display="inline-block"
         sx={(theme) => ({
-          backgroundImage: theme.fn.gradient({ from: 'teal', to: 'blue', deg: 45 }),
-          '-webkit-background-clip': 'text',
-          '-webkit-text-fill-color': 'transparent',
-        })}>
-        <Link to="/" aria-label='go to home' style={{ textDecoration: 'none' }}>
-          <Text c="teal" fw={700} component='span'>
+          backgroundImage: theme.fn.gradient({
+            from: "teal",
+            to: "blue",
+            deg: 45,
+          }),
+          "-webkit-background-clip": "text",
+          "-webkit-text-fill-color": "transparent",
+        })}
+      >
+        <Link to="/" aria-label="go to home" style={{ textDecoration: "none" }}>
+          <Text c="teal" fw={700} component="span">
             {title}
           </Text>
         </Link>
       </Box>
-      <Link to="/contact" aria-label='go to contact page'>
-        <IconMail color='steelblue' />
-      </Link>
+      <Box display="flex" sx={{ justifyContent: "space-between" }}>
+        <Box mr={6} sx={{ fontSize: 12, lineHeight: 2 }}>
+          <Link to="/" aria-label="English" style={{ textDecoration: "none" }}>
+            <Text c="steelblue" fw={700} component="span">
+              EN
+            </Text>
+          </Link>
+          <Text c="steelblue" mx={3} component="span">
+            /
+          </Text>
+          <Link
+            to="/ja"
+            aria-label="Japanaese"
+            style={{ textDecoration: "none" }}
+          >
+            <Text c="steelblue" fw={700} component="span">
+              JA
+            </Text>
+          </Link>
+        </Box>
+        <Link to="/contact" aria-label="go to contact page">
+          <IconMail color="steelblue" />
+        </Link>
+      </Box>
     </MantineHeader>
   );
 };
