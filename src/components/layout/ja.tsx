d765f8a25fc476e61d851d076e00477cf59558f8
tyamap <tom333my@outlook.com>
2024-01-24 21:09:11 +0900

    ui: テーマ切り替えボタンなどヘッダーのボタンを整備

--- src/components/layout/ja.tsx
+++ src/components/layout/ja.tsx
@@ -1,33 +1,42 @@
-import { Container, Grid } from "@mantine/core"
-import { MantineProvider } from '@mantine/core';
-import { theme } from 'src/theme'
+import { ColorScheme, ColorSchemeProvider, Container, Grid } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "src/theme";
 import Header from "src/components/layout/ja/Header";
 import Aside from "src/components/layout/ja/Aside";
 import Main from "src/components/layout/ja/Main";
 import Tabs from "src/components/layout/ja/Tabs";
+import { useState } from "react";
 
 type LayoutProps = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 export const Layout = ({ children }: LayoutProps) => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{ colorScheme: "dark" }}
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <Header />
-      <Container size="xl" pt={50} px={0}>
-        <Grid w="100%" m={0}>
-          <Grid.Col xs={12} sm={3}>
-            <Aside />
-          </Grid.Col>
-          <Grid.Col span="auto">
-            <Tabs />
-            <Main>{children}</Main>
-          </Grid.Col>
-        </Grid>
-      </Container>
-    </MantineProvider>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme, ...theme }}
+      >
+        <Header />
+        <Container size="xl" pt={50} px={0}>
+          <Grid w="100%" m={0}>
+            <Grid.Col xs={12} sm={3}>
+              <Aside />
+            </Grid.Col>
+            <Grid.Col span="auto">
+              <Tabs />
+              <Main>{children}</Main>
+            </Grid.Col>
+          </Grid>
+        </Container>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
-}
+};
